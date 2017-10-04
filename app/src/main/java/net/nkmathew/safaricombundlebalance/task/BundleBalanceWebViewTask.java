b94ed0e5235d20a2ac13dbc624b301b3f61b09f6
nkmathew <kipkoechmathew@gmail.com>
2017-10-04 15:25:47 +0300

    Optimize imports

--- app/src/main/java/net/nkmathew/safaricombundlebalance/task/BundleBalanceWebViewTask.java
+++ app/src/main/java/net/nkmathew/safaricombundlebalance/task/BundleBalanceWebViewTask.java
@@ -2,15 +2,10 @@ package net.nkmathew.safaricombundlebalance.task;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
-import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
