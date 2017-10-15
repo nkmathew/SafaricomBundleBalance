00e2b1821baaaeb37e0e592fee5c932e4045f568
nkmathew <kipkoechmathew@gmail.com>
2017-10-15 21:05:42 +0300

    Class for app constants

---
+++ app/src/main/java/net/nkmathew/safaricombundlebalance/utils/Constants.java
@@ -0,0 +1,13 @@
+package net.nkmathew.safaricombundlebalance.utils;
+
+/*
+ * Created by nkmathew on 09/10/2017.
+ */
+
+import okhttp3.MediaType;
+
+public abstract class Constants {
+
+    public static final String ENDPOINT_BUNDLES = "http://www.safaricom.com/bundles/GetSubDetails";
+    public static final int ID_BUNDLE_CHECK_ALARM = 666;
+}
